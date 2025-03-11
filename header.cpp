faf4f07ffd4aa78fccb120b79a0f27c39b6ee30e
VityaRolex <gabarkartoshkin@tuta.io>
2025-03-11 13:09:12 +0300

    added function CountElements

---
+++ header.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+int32_t CountElements(char * Arr, int32_t lenght)
+{
+    int32_t result{};
+    int32_t index_of_first_zero{};
+    int32_t index_of_last_zero{};
+    bool is_zero_first{1};
+    for (size_t i = 0; i < lenght; ++i)
+    {
+        if (Arr[i] == '0' && is_zero_first)
+        {
+            index_of_first_zero = i;
+            is_zero_first = 0;
+        }
+        if (Arr[i] == '0')
+        {
+            index_of_last_zero = i;
+        }
+    }
+    return index_of_last_zero - index_of_first_zero;
+}
+
+
+int32_t CountElements(double * Arr, int32_t lenght)
+{
+    int32_t result{};
+    int32_t index_of_first_zero{};
+    int32_t index_of_last_zero{};
+    bool is_zero_first{1};
+    for (size_t i = 0; i < lenght; ++i)
+    {
+        if (Arr[i] == 0 && is_zero_first)
+        {
+            index_of_first_zero = i;
+            is_zero_first = 0;
+        }
+        if (Arr[i] == 0)
+        {
+            index_of_last_zero = i;
+        }
+    }
+    return index_of_last_zero - index_of_first_zero;
+}
+
+
+int32_t CountElements(int32_t * Arr, int32_t lenght)
+{
+    int32_t result{};
+    int32_t index_of_first_zero{};
+    int32_t index_of_last_zero{};
+    bool is_zero_first{1};
+    for (size_t i = 0; i < lenght; ++i)
+    {
+        if (Arr[i] == 0 && is_zero_first)
+        {
+            index_of_first_zero = i;
+            is_zero_first = 0;
+        }
+        if (Arr[i] == 0)
+        {
+            index_of_last_zero = i;
+        }
+    }
+    return index_of_last_zero - index_of_first_zero;
+}
---
+++ header.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int32_t CountElements(char * , int32_t );
+int32_t CountElements(double * , int32_t);
+int32_t CountElements(int32_t * , int32_t);
