6309f5b9ccbfb2c3be76d003f30ba5043d6248de
Sta4ik <ctac354.354@gmail.com>
2025-03-11 20:05:05 +0300

    Add ReverseArray, FindElement, CalculateAverage

--- header.h
+++ header.h
@@ -1,2 +1,29 @@
 #include <iostream>
 
+int64_t FindMaxElement(int64_t*);
+int64_t FindMinElement(int64_t*);
+int64_t CalculateAverage(int64_t*);
+
+template<typename Type>
+Type FindElement(Type* arr, Type element)
+{
+    int64_t size = sizeof(arr)/sizeof(arr[0]);
+    for(size_t i = 0; i <= size; ++i)
+    {
+        if(arr[i] == element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+template<typename Type>
+void ReverseArray(Type* arr)
+{
+    int64_t size = sizeof(arr)/sizeof(arr[0]);
+    for(size_t i = 0; i <= size/2; ++i)
+    {
+        switch(arr[i], arr[size - i]);
+    }
+}
