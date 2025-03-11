43c58fa92e9557a6d8379ab57616b1fc41934c03
VityaRolex <gabarkartoshkin@tuta.io>
2025-03-11 16:53:56 +0300

    added CMakeLists and Bubble sort

--- header.h
+++ header.h
@@ -7,12 +7,15 @@ int32_t outputSumOfSimpleElements(int32_t*, int32_t);
 template<typename Type>
 void BubbleSort(Type * Arr, int32_t lenght)
 {
+    Type temp{};
     int32_t counter{};
-    while(counter < lenght)
+    while(counter < lenght - 1)
     {
         if (Arr[counter] > Arr[counter + 1])
         {
-            swap(Arr[counter], Arr[counter + 1]);
+            temp = Arr[counter];
+            Arr[counter] = Arr[counter+1];
+            Arr[counter + 1] = temp;
             counter = -1;
         }
         counter += 1;
