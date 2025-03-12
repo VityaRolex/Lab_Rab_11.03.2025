a1e317ad00e1c660611df39f36103744c28e677d
VityaRolex <gabarkartoshkin@tuta.io>
2025-03-12 22:48:52 +0300

    added functions 1, 4, 9

--- header.cpp
+++ header.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "header.h"
+#include <cmath>
 
 void PrintArray(int64_t* arr, int64_t size)
 {
@@ -63,4 +64,138 @@ void RemoveNegativesAndFillZeros(int64_t* arr, int64_t size)
  }
 }
 
+int32_t CountElements(char * Arr, int32_t lenght)
+{
+    int32_t result{};
+    int32_t index_of_first_zero{};
+    int32_t index_of_last_zero{};
+    bool is_zero_first{1};
+    for (size_t i = 0; i < lenght; ++i)
+    {
+        if (Arr[i] == '0' && is_zero_first)
+        {
+            index_of_first_zero = i;
+            is_zero_first = 0;
+        }
+        if (Arr[i] == '0')
+        {
+            index_of_last_zero = i;
+        }
+    }
+    if (is_zero_first || index_of_last_zero- index_of_first_zero < 0)
+    {
+        return -1;
+    }
+    return index_of_last_zero - index_of_first_zero;
+}
 
+
+int32_t CountElements(double * Arr, int32_t lenght)
+{
+    int32_t result{};
+    int32_t index_of_first_zero{};
+    int32_t index_of_last_zero{};
+    bool is_zero_first{1};
+    for (size_t i = 0; i < lenght; ++i)
+    {
+        if (Arr[i] == 0 && is_zero_first)
+        {
+            index_of_first_zero = i;
+            is_zero_first = 0;
+        }
+        if (Arr[i] == 0)
+        {
+            index_of_last_zero = i;
+        }
+    }
+    if (is_zero_first || index_of_last_zero- index_of_first_zero < 0)
+    {
+        return -1;
+    }
+    return index_of_last_zero - index_of_first_zero;
+}
+
+
+int32_t CountElements(int32_t * Arr, int32_t lenght)
+{
+    int32_t result{};
+    int32_t index_of_first_zero{};
+    int32_t index_of_last_zero{};
+    bool is_zero_first{1};
+    for (size_t i = 0; i < lenght; ++i)
+    {
+        if (Arr[i] == 0 && is_zero_first)
+        {
+            index_of_first_zero = i;
+            is_zero_first = 0;
+        }
+        if (Arr[i] == 0)
+        {
+            index_of_last_zero = i;
+        }
+    }
+    if (is_zero_first || index_of_last_zero- index_of_first_zero < 0)
+    {
+        return -1;
+    }
+    return index_of_last_zero - index_of_first_zero;
+}
+
+
+bool isNumberSimple(int32_t number)
+{
+    for (size_t i = 2; i < sqrt(number); ++i)
+    {
+        if (number % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+int32_t outputSumOfSimpleElements(int32_t* Arr, int32_t lenght)
+{
+    int32_t result{};
+    for (size_t i = 0; i < lenght; ++i)
+    {
+        if (isNumberSimple(Arr[i]))
+        {
+            result += Arr[i];
+        }
+    }
+    return result;
+}
+int64_t FindMaxElement(int64_t* arr)
+{
+    int64_t maxElement{};
+    int64_t size = sizeof(arr)/sizeof(arr[0]);
+    for (size_t i = 0; i <= size; ++i) 
+    {
+        if (arr[i] > maxElement) 
+        {
+            maxElement = arr[i];
+        }
+    }
+    return maxElement;
+}
+
+int64_t FindMinElement(int64_t* arr)
+{
+    int64_t minElement{};
+    int64_t size = sizeof(arr)/sizeof(arr[0]);
+    for (size_t i = 0; i <= size; ++i) 
+    {
+        if (arr[i] < minElement) 
+        {
+            minElement = arr[i];
+        }
+    }
+    return minElement;
+}
+
+int64_t CalculateAverage(int64_t* arr)
+{
+    return (FindMaxElement(arr) + FindMinElement(arr)) / 2;
+}
--- header.h
+++ header.h
@@ -8,4 +8,53 @@ void InputSize(int64_t&);
 int64_t MaxElement(int64_t*, int64_t);
 int64_t MinElement(int64_t*, int64_t);
 void RemoveNegativesAndFillZeros(int64_t*, int64_t);
+int32_t CountElements(char * , int32_t );
+int32_t CountElements(double * , int32_t);
+int32_t CountElements(int32_t * , int32_t);
+int32_t outputSumOfSimpleElements(int32_t*, int32_t);
+template<typename Type>
+void BubbleSort(Type * Arr, int32_t lenght)
+{
+    Type temp{};
+    int32_t counter{};
+    while(counter < lenght - 1)
+    {
+        if (Arr[counter] > Arr[counter + 1])
+        {
+            temp = Arr[counter];
+            Arr[counter] = Arr[counter+1];
+            Arr[counter + 1] = temp;
+            counter = -1;
+        }
+        counter += 1;
+    }
+}
+
+int64_t FindMaxElement(int64_t*);
+int64_t FindMinElement(int64_t*);
+int64_t CalculateAverage(int64_t*);
+
+template<typename Type>
+Type FindElement(Type* arr, Type element)
+{
+    int64_t size = sizeof(arr)/sizeof(arr[0]);
+    for(size_t i = 0; i <= size; ++i)
+    {
+        if(arr[i] == element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+template<typename Type>
+void ReverseArray(Type* arr)
+{
+    int64_t size = sizeof(arr)/sizeof(arr[0]);
+    for(size_t i = 0; i <= size/2; ++i)
+    {
+        switch(arr[i], arr[size - i]);
+    }
+}
 #endif // HEADER.H
