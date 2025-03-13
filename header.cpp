2c9f6914cff84103c6871b38fecce19978024224
VityaRolex <gabarkartoshkin@tuta.io>
2025-03-13 21:47:02 +0300

    a

--- header.cpp
+++ header.cpp
@@ -91,58 +91,6 @@ int32_t CountElements(char * Arr, int32_t lenght)
 }
 
 
-int32_t CountElements(double * Arr, int32_t lenght)
-{
-    int32_t result{};
-    int32_t index_of_first_zero{};
-    int32_t index_of_last_zero{};
-    bool is_zero_first{1};
-    for (size_t i = 0; i < lenght; ++i)
-    {
-        if (Arr[i] == 0 && is_zero_first)
-        {
-            index_of_first_zero = i;
-            is_zero_first = 0;
-        }
-        if (Arr[i] == 0)
-        {
-            index_of_last_zero = i;
-        }
-    }
-    if (is_zero_first || index_of_last_zero- index_of_first_zero < 0)
-    {
-        return -1;
-    }
-    return index_of_last_zero - index_of_first_zero;
-}
-
-
-int32_t CountElements(int32_t * Arr, int32_t lenght)
-{
-    int32_t result{};
-    int32_t index_of_first_zero{};
-    int32_t index_of_last_zero{};
-    bool is_zero_first{1};
-    for (size_t i = 0; i < lenght; ++i)
-    {
-        if (Arr[i] == 0 && is_zero_first)
-        {
-            index_of_first_zero = i;
-            is_zero_first = 0;
-        }
-        if (Arr[i] == 0)
-        {
-            index_of_last_zero = i;
-        }
-    }
-    if (is_zero_first || index_of_last_zero- index_of_first_zero < 0)
-    {
-        return -1;
-    }
-    return index_of_last_zero - index_of_first_zero;
-}
-
-
 bool isNumberSimple(int32_t number)
 {
     for (size_t i = 2; i < sqrt(number); ++i)
@@ -195,7 +143,7 @@ int64_t FindMinElement(int64_t* arr, int64_t size)
 }
 
 int64_t CalculateAverage(int64_t* arr, int64_t size)
-}
+{
     return (FindMaxElement(arr, size) + FindMinElement(arr, size)) / 2.0;
 }
 
